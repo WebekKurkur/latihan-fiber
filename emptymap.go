15c5d80ebbdc91cb8e3a605e774af6a653e3dc8d
Helpdesk Dev <makanbangku111@gmail.com>
2026-08-19 15:26:12 +0700

    tugas 2: Operasi pada map: menambah, membaca dengan pengecekan keberadaan, menghapus, dan menelusuri seluruh isinya

--- emptymap.go
+++ emptymap.go
@@ -1,10 +1,34 @@
 package main
 
+import "fmt"
+
 func main() {
+	//inisialisasi map
 	nilaiMhs := make(map[string]int)
 
+	//a. menambah isi map
 	nilaiMhs["Raka"] = 85
 	nilaiMhs["Habib"] = 92
 	nilaiMhs["Irsyad"] = 78
 	nilaiMhs["Farel"] = 88
+	//print isi map
+	fmt.Println(nilaiMhs)
+
+	//b. read map dengan pengecekan pengecekan keberadaan
+	target := "Raka"
+	if nilai, exists := nilaiMhs[target]; exists {
+		fmt.Printf("nilainya %s: %d\n", target, nilai)
+	} else {
+		fmt.Printf("Data mahasiswa tidak ditemukan.", target)
+	}
+
+	//c. menghapus data dari map
+	delete(nilaiMhs, "Irsyad")
+	fmt.Println("data 'Irsyad' telah dihapus, sisa:", nilaiMhs)
+
+	//d. menelusuri seluruh isi map
+	fmt.Println("\nMenelusuri seluruh isi map tersisa:")
+	for namaMhs, nilaiAkhir := range nilaiMhs {
+		fmt.Printf("- Mahasiswa: %s , Nilai: %d\n", namaMhs, nilaiAkhir)
+	}
 }
